2aa585a47db9b45889f6d98772bfbd54544b708c
pykwon <pykwon@hanmail.net>
2024-07-03 14:13:26 +0900

    spring 시작 : DI

---
+++ spr1/src/main/java/pack/Message2.java
@@ -0,0 +1,9 @@
+package pack;
+
+public class Message2 implements MessageInter{
+	@Override
+	public void sayHello(String name) {
+		String imsi = name + "씨";
+		System.out.println("반가워요 " + imsi);
+	}
+}
