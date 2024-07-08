86130e817004b8c9d13812dff2b66eafcf64a164
pykwon <pykwon@hanmail.net>
2024-07-08 11:31:54 +0900

    aop test

---
+++ spr12aop_login/src/main/java/spr12aop_login/MyAdvice.java
@@ -0,0 +1,32 @@
+package spr12aop_login;
+
+import java.util.Scanner;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyAdvice {
+	@Around("execution(public void startProcess())")
+	public Object abc(ProceedingJoinPoint joinPoint) throws Throwable{
+		
+		System.out.println("AOP 시작 : 핵심 로직 수행 전 id 검증");
+		
+		System.out.print("input id:");
+		Scanner scanner = new Scanner(System.in);
+		String id = scanner.nextLine();
+		scanner.close();
+		
+		if(!id.equals("ok")) {
+			System.out.println("id 불일치! 작업을 종료합니다");
+			return null;
+		}
+		
+		Object object = joinPoint.proceed();
+		
+		return object;
+	}
+}
